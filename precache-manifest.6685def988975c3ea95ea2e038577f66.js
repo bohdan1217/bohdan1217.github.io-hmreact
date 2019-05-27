ff323290fb799ccdd3ec2c257f9a0f0bdf49299f
Богдан Гордійчук <bogdangordijcuk@Bogdans-MacBook-Pro.local>
2019-05-28 00:07:55 +0300

    first commit2

---
+++ precache-manifest.6685def988975c3ea95ea2e038577f66.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f9ee4b394f27ae6db39c454006c8eced",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a28035d73247d243f54f",
+    "url": "/static/js/2.e7b26226.chunk.js"
+  },
+  {
+    "revision": "32feca101842fef9ca92",
+    "url": "/static/js/main.7fef65e3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
